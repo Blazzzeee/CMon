85f0083e4d539dcdf76a6c8fcfb78641b0205449
Blazzzeee <legendgamer1007@gmail.com>
2026-01-16 19:39:47 +0530

    add dynamic linking to libevent

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+echo "Starting build phase"
 set -euo pipefail
-target=test.c
+target=main.c
 
-cc -Wall -Wextra -g -o ./target "$target"
+cc -Wall -Wextra -g -o ./target "$target" $(pkg-config --cflags --libs libevent)
 
 echo "Compilation Successfull"
+
+echo "Starting Server"
 ./target
