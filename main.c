3687413a2c6200857ce187f9d5daa4b27c812174
Blazzzeee <legendgamer1007@gmail.com>
2026-01-16 19:40:01 +0530

    init server with generic replies

--- main.c
+++ main.c
@@ -4,44 +4,59 @@
 #include <event2/event.h>
 #include <event2/http.h>
 
-static void
-generic_request_handler(struct evhttp_request *req, void *ctx)
-{
-        struct evbuffer *reply = evbuffer_new();
+#define PORT 8000
 
-        evbuffer_add_printf(reply, "It works!");
-        evhttp_send_reply(req, HTTP_OK, NULL, reply);
-        evbuffer_free(reply);
+
+static void generic_request_handler(struct evhttp_request *req, void *ctx)
+{ 
+    // This is a generic callback invoked by the server 
+    // on a http request , 
+    // TODO: this method should refuse unsupportted routes , unsupportted methods 
+    // TODO: Redirect the callback to appropriate handler
+    struct evbuffer *reply = evbuffer_new();
+
+    evbuffer_add_printf(reply, "It works!");
+    evhttp_send_reply(req, HTTP_OK, NULL, reply);
+    evbuffer_free(reply);
 }
 
 static void signal_cb(evutil_socket_t fd, short event, void *arg)
 {
-        printf("%s signal received\n", strsignal(fd));
-        event_base_loopbreak(arg);
+    printf("%s Shutting down server...\n", strsignal(fd));
+    event_base_loopbreak(arg);
 }
 
 int main()
 {
-        ev_uint16_t http_port = 8080;
-        char *http_addr = "0.0.0.0";
-        struct event_base *base;
-        struct evhttp *http_server;
-        struct event *sig_int;
+    ev_uint16_t http_port = PORT;
+    char *http_addr = "0.0.0.0";
+    struct event_base *base;
+    struct evhttp *http_server;
+    struct event *sig_int;
 
-        base = event_base_new();
+    base = event_base_new();
 
-        http_server = evhttp_new(base);
-        evhttp_bind_socket(http_server, http_addr, http_port);
-        evhttp_set_gencb(http_server, generic_request_handler, NULL);
+    http_server = evhttp_new(base);
+    // Bind the server to system socket
+    evhttp_bind_socket(http_server, http_addr, http_port);
+    // Register generic http callback
+    evhttp_set_gencb(http_server, generic_request_handler, NULL);
 
-        sig_int = evsignal_new(base, SIGINT, signal_cb, base);
-        event_add(sig_int, NULL);
 
-        printf("Listening requests on http://%s:%d\n", http_addr, http_port);
+    // Handle SIGINT and in future and SIGKILL here
+    sig_int = evsignal_new(base, SIGINT, signal_cb, base);
+    event_add(sig_int, NULL);
 
-        event_base_dispatch(base);
 
-        evhttp_free(http_server);
-        event_free(sig_int);
-        event_base_free(base);
+
+    printf("Listening requests on http://%s:%d\n", http_addr, http_port);
+
+
+    // Start the server event loop
+    event_base_dispatch(base);
+
+    // Teardown
+    evhttp_free(http_server);
+    event_free(sig_int);
+    event_base_free(base);
 }
