c34ab7c4346e7a4903769c55038e189fa3813232
Blazzzeee <legendgamer1007@gmail.com>
2026-01-17 21:52:31 +0530

    add debugger options to build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,16 @@
 echo "Starting build phase"
 set -euo pipefail
 target=main.c
+DEBUG=${DEBUG:-0}
 
 cc -Wall -Wextra -g -o ./target "$target" $(pkg-config --cflags --libs libevent)
 
 echo "Compilation Successfull"
 
-echo "Starting Server"
-./target
+if [ "$DEBUG" -eq 0 ]; then
+    echo "Starting server without debugger"
+    ./target
+else
+    echo "Starting server inside gdb"
+    gdb --args ./target
+fi
