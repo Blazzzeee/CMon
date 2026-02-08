d559ccb8366903a0c0f2523947105f1628e6a9cd
Blazzzeee <legendgamer1007@gmail.com>
2026-02-08 13:03:04 +0530

    Fix arena.h

--- arena.h
+++ arena.h
@@ -1,29 +1,27 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <unistd.h>
+#ifndef ARENA_H
+#define ARENA_H
 
-// Individual buffer size is 256 bytes each
-#define BUF_SIZE 256
-// Define number of buffers
-// since we associate each bit of
-// 64 bit integer to each buffer 
+#include <stddef.h>
+#include <stdint.h>
+
+#define BUF_SIZE (256) 
+
 #define BUF_NUM 64
 
-
-// Used to initialise the arena
-// performs all the necessary operations that will be needed
-// before allocationg  a request
+#define BITMAP_WORDS (BUF_NUM / 64)
+// Initializes the arena
 void *prealloc_arena();
 
-
+// Allocates a buffer of 'req' bytes (max BUF_SIZE)
 void *allocate(size_t req);
 
-
+// Deallocates a pointer
 void deallocate(void *ptr);
 
-
+// Tears down execution
 void teardown_arena();
 
-void *check_and_claim_atomically(size_t req);
-
 int find_k_consecutive_zeroes(int k);
+
+
+#endif
